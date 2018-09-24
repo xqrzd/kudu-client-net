3925b83369e774148a0e91a9bde2200c57f1ee74
xqrzd <4950876+xqrzd@users.noreply.github.com>
2018-09-23 23:10:33 -0400

    Add ServerInfo class
    Container class for server information that
    never changes, like UUID and hostname.

---
+++ src/Kudu.Client/Connection/ServerInfo.cs
@@ -0,0 +1,20 @@
+﻿namespace Kudu.Client.Connection
+{
+    /// <summary>
+    /// Container class for server information that never changes, like UUID and hostname.
+    /// </summary>
+    public class ServerInfo
+    {
+        public string Uuid { get; }
+
+        public HostAndPort HostPort { get; }
+
+        public ServerInfo(string uuid, HostAndPort hostPort)
+        {
+            Uuid = uuid;
+            HostPort = hostPort;
+        }
+
+        public override string ToString() => $"{Uuid} ({HostPort})";
+    }
+}
