73ede5a61e9463ba5629ed2f1564adbdcee4a96f
xqrzd <4950876+xqrzd@users.noreply.github.com>
2018-09-25 21:11:15 -0400

    [WIP] Connection cache

---
+++ src/Kudu.Client/Connection/ConnectionCache.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Kudu.Client.Connection
+{
+    public class ConnectionCache : IDisposable
+    {
+        private Dictionary<HostAndPort, Task<KuduConnection>> _connections;
+
+        public ConnectionCache()
+        {
+            _connections = new Dictionary<HostAndPort, Task<KuduConnection>>();
+        }
+
+        public Task<KuduConnection> CreateConnectionAsync(HostAndPort hostPort)
+        {
+            lock (_connections)
+            {
+                if (_connections.TryGetValue(hostPort, out var connection))
+                    return connection;
+                else
+                {
+                    var task = KuduConnectionFactory.ConnectAsync(hostPort);
+                    _connections.Add(hostPort, task);
+                    return task;
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            foreach (var connection in _connections.Values)
+            {
+                try
+                {
+                    connection.GetAwaiter().GetResult().Dispose();
+                }
+                catch
+                {
+                    // TODO
+                    throw;
+                }
+            }
+        }
+    }
+}
---
+++ src/Kudu.Client/Connection/KuduConnectionFactory.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Kudu.Client.Negotiate;
+
+namespace Kudu.Client.Connection
+{
+    // TODO: abstract this
+    public class KuduConnectionFactory
+    {
+        public static async Task<KuduConnection> ConnectAsync(HostAndPort hostPort)
+        {
+            var connection = await KuduSocketConnection.ConnectAsync(hostPort);
+            var kuduConnection = new KuduConnection(connection);
+            var negotiator = new Negotiator(kuduConnection);
+            await negotiator.NegotiateAsync();
+
+            return kuduConnection;
+        }
+    }
+}
