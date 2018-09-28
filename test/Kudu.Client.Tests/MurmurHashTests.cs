eb861b724e87908a0dcd3b57eb9bdf70c0a56e2a
xqrzd <4950876+xqrzd@users.noreply.github.com>
2018-09-27 22:20:41 -0400

    Add 64bit Murmur2 hash and some tests

---
+++ test/Kudu.Client.Tests/MurmurHashTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+using Kudu.Client.Util;
+using Xunit;
+
+namespace Kudu.Client.Tests
+{
+    public class MurmurHashTests
+    {
+        [Fact]
+        public void TestMurmur2Hash64()
+        {
+            ulong hash;
+
+            hash = Murmur2.Hash64(Encoding.UTF8.GetBytes("ab"), 0);
+            Assert.Equal(7115271465109541368UL, hash);
+
+            hash = Murmur2.Hash64(Encoding.UTF8.GetBytes("abcdefg"), 0);
+            Assert.Equal(2601573339036254301UL, hash);
+
+            hash = Murmur2.Hash64(Encoding.UTF8.GetBytes("quick brown fox"), 42);
+            Assert.Equal(3575930248840144026UL, hash);
+        }
+    }
+}
