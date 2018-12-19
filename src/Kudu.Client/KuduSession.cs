0e31051f1f03dea016a2c57b0c44e8a30a20e1e7
xqrzd <4950876+xqrzd@users.noreply.github.com>
2018-12-18 23:02:49 -0500

    fix build

--- src/Kudu.Client/KuduSession.cs
+++ src/Kudu.Client/KuduSession.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Channels;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Kudu.Client.Protocol.Tserver;
 
