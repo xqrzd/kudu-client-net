289f4ac73b0a25ca60e9c98d9f34ba6bbfd9db45
xqrzd <4950876+xqrzd@users.noreply.github.com>
2020-01-13 18:47:39 -0500

    Import FastHash hash function to util (#34)
    FastHash is a simple, robust, and efficient general purpose
    hash function from Google.
    
    It's fast, small in size and with least/no quality problems
    as per smhasher and hence a good candidate for BlockBloomFilter.
    
    Implementation has been ported straight from
    https://code.google.com/archive/p/fast-hash/
    
    Based on Kudu commit 5067abbdd2dc95eea74072dec1b07e3c0eb98423

---
+++ test/Knet.Kudu.Client.Tests/FastHashTests.cs
@@ -0,0 +1,28 @@
+﻿using Knet.Kudu.Client.Util;
+using Xunit;
+
+namespace Knet.Kudu.Client.Tests
+{
+    public class FastHashTests
+    {
+        [Theory]
+        [InlineData("ab", 0, 17293172613997361769UL)]
+        [InlineData("abcdefg", 0, 10206404559164245992UL)]
+        [InlineData("quick brown fox", 42, 3757424404558187042UL)]
+        public void TestFastHash64(string data, ulong seed, ulong expectedHash)
+        {
+            ulong hash = FastHash.Hash64(data.ToUtf8ByteArray(), seed);
+            Assert.Equal(expectedHash, hash);
+        }
+
+        [Theory]
+        [InlineData("ab", 0, 2564147595U)]
+        [InlineData("abcdefg", 0, 1497700618U)]
+        [InlineData("quick brown fox", 42, 1676541068U)]
+        public void TestFastHash32(string data, uint seed, uint expectedHash)
+        {
+            uint hash = FastHash.Hash32(data.ToUtf8ByteArray(), seed);
+            Assert.Equal(expectedHash, hash);
+        }
+    }
+}
