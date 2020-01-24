0708df0aa1f37555e50b40ac6bda8f6e3b433fc6
xqrzd <4950876+xqrzd@users.noreply.github.com>
2020-01-23 23:17:24 -0500

    Throw exception when operating on a partition that doesn't exist

---
+++ src/Knet.Kudu.Client/Exceptions/NonCoveredRangeException.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Knet.Kudu.Client.Exceptions
+{
+    /// <summary>
+    /// Exception indicating that an operation attempted to access a
+    /// non-covered range partition.
+    /// </summary>
+    public class NonCoveredRangeException : NonRecoverableException
+    {
+        public byte[] NonCoveredRangeStart;
+
+        public byte[] NonCoveredRangeEnd;
+
+        public NonCoveredRangeException(
+            byte[] nonCoveredRangeStart,
+            byte[] nonCoveredRangeEnd)
+            : base(KuduStatus.NotFound(GetMessage(
+                nonCoveredRangeStart, nonCoveredRangeEnd)))
+        {
+            NonCoveredRangeStart = nonCoveredRangeStart;
+            NonCoveredRangeEnd = nonCoveredRangeEnd;
+        }
+
+        private static string GetMessage(byte[] start, byte[] end)
+        {
+            var startStr = start.Length == 0 ? "<start>" : BitConverter.ToString(start);
+            var endStr = end.Length == 0 ? "<end>" : BitConverter.ToString(end);
+
+            return $"[{startStr}, {endStr})";
+        }
+    }
+}
---
+++ src/Knet.Kudu.Client/Tablet/RemoteTabletExtensions.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Knet.Kudu.Client.Tablet
+{
+    public static class RemoteTabletExtensions
+    {
+        public static RemoteTablet FindTablet(
+            this List<RemoteTablet> tablets, ReadOnlySpan<byte> partitionKey)
+        {
+            int lo = 0;
+            int hi = tablets.Count - 1;
+            // If length == 0, hi == -1, and loop will not be entered
+            while (lo <= hi)
+            {
+                // PERF: `lo` or `hi` will never be negative inside the loop,
+                //       so computing median using uints is safe since we know
+                //       `length <= int.MaxValue`, and indices are >= 0
+                //       and thus cannot overflow an uint.
+                //       Saves one subtraction per loop compared to
+                //       `int i = lo + ((hi - lo) >> 1);`
+                int i = (int)(((uint)hi + (uint)lo) >> 1);
+
+                RemoteTablet tablet = tablets[i];
+                int c = partitionKey.SequenceCompareTo(tablet.Partition.PartitionKeyStart);
+                if (c == 0)
+                {
+                    return tablet;
+                }
+                else if (c > 0)
+                {
+                    lo = i + 1;
+                }
+                else
+                {
+                    hi = i - 1;
+                }
+            }
+
+            if (hi >= 0)
+            {
+                RemoteTablet tablet = tablets[hi];
+
+                if (tablet.Partition.ContainsPartitionKey(partitionKey))
+                {
+                    return tablet;
+                }
+            }
+
+            return null;
+        }
+
+        internal static byte[] GetNonCoveredRangeEnd(
+            this List<RemoteTablet> tablets, ReadOnlySpan<byte> partitionKey)
+        {
+            // Inefficient search, but this is only used to display
+            // more info in a NonCoveredRangeException.
+            foreach (var tablet in tablets)
+            {
+                var start = tablet.Partition.PartitionKeyStart;
+
+                if (partitionKey.SequenceCompareTo(start) < 0)
+                    return start;
+            }
+
+            return Array.Empty<byte>();
+        }
+    }
+}
