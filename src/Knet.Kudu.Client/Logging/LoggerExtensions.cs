5ed33b913ca2a83c43d598ddad4dad441371f3d0
xqrzd <4950876+xqrzd@users.noreply.github.com>
2020-04-03 19:23:18 -0400

    Remove unused log message

--- src/Knet.Kudu.Client/Logging/LoggerExtensions.cs
+++ src/Knet.Kudu.Client/Logging/LoggerExtensions.cs
@@ -12,7 +12,6 @@ namespace Knet.Kudu.Client.Logging
     public static class LoggerExtensions
     {
         private static readonly Action<ILogger, HostAndPort, IPAddress, string, string, bool, Exception> _connectedToServer;
-        private static readonly Action<ILogger, Exception> _unableToFindLeaderMaster;
         private static readonly Action<ILogger, int, string, int, string, Exception> _misconfiguredMasterAddresses;
         private static readonly Action<ILogger, string, Exception> _connectionDisconnected;
         private static readonly Action<ILogger, HostAndPort, IPAddress, string, Exception> _unableToConnectToServer;
@@ -27,38 +26,33 @@ namespace Knet.Kudu.Client.Logging
                 logLevel: LogLevel.Debug,
                 formatString: "Connected to {HostPort}; Ip: {Ip}; over {Tls}; with {NegotiateInfo}; IsLocal: {IsLocal}");
 
-            _unableToFindLeaderMaster = LoggerMessage.Define(
-                eventId: new EventId(2, "UnableToFindLeaderMaster"),
-                logLevel: LogLevel.Warning,
-                formatString: "Unable to find a leader master");
-
             _misconfiguredMasterAddresses = LoggerMessage.Define<int, string, int, string>(
-                eventId: new EventId(3, "MisconfiguredMasterAddresses"),
+                eventId: new EventId(2, "MisconfiguredMasterAddresses"),
                 logLevel: LogLevel.Warning,
                 formatString: "Client configured with {NumClientMasters} master(s) ({ClientMasters}) but cluster indicates it expects {NumClusterMasters} master(s) ({ClusterMasters})");
 
             _connectionDisconnected = LoggerMessage.Define<string>(
-                eventId: new EventId(4, "ConnectionDisconnected"),
+                eventId: new EventId(3, "ConnectionDisconnected"),
                 logLevel: LogLevel.Warning,
                 formatString: "Connection ungracefully closed: {Server}");
 
             _unableToConnectToServer = LoggerMessage.Define<HostAndPort, IPAddress, string>(
-                eventId: new EventId(5, "UnableToConnectToServer"),
+                eventId: new EventId(4, "UnableToConnectToServer"),
                 logLevel: LogLevel.Warning,
                 formatString: "Unable to connect to {HostPort}; Ip: {Ip}; UUID: {UUID}");
 
             _exceptionSendingSessionData = LoggerMessage.Define(
-                eventId: new EventId(6, "ExceptionSendingSessionData"),
+                eventId: new EventId(5, "ExceptionSendingSessionData"),
                 logLevel: LogLevel.Error,
                 formatString: "Exception occurred while flushing session data, will retry");
 
             _exceptionClosingScanner = LoggerMessage.Define(
-                eventId: new EventId(7, "ExceptionClosingScanner"),
+                eventId: new EventId(6, "ExceptionClosingScanner"),
                 logLevel: LogLevel.Warning,
                 formatString: "Exception occurred while closing scanner");
 
             _recoverableRpcException = LoggerMessage.Define(
-                eventId: new EventId(8, "RecoverableRpcException"),
+                eventId: new EventId(7, "RecoverableRpcException"),
                 logLevel: LogLevel.Warning,
                 formatString: "RPC failed, will retry");
         }
@@ -66,11 +60,6 @@ namespace Knet.Kudu.Client.Logging
         public static void ConnectedToServer(this ILogger logger, HostAndPort hostPort, IPAddress ipAddress, string tlsInfo, string negotiateInfo, bool isLocal)
         {
             _connectedToServer(logger, hostPort, ipAddress, tlsInfo, negotiateInfo, isLocal, null);
-        }
-
-        public static void UnableToFindLeaderMaster(this ILogger logger)
-        {
-            _unableToFindLeaderMaster(logger, null);
         }
 
         public static void MisconfiguredMasterAddresses(this ILogger logger, IReadOnlyList<HostAndPort> clientMasters, List<HostPortPB> clusterMasters)
