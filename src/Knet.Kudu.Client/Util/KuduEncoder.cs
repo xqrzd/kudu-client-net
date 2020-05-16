63b5186b55e14aa7d151340dd12c0e0a3ae82402
xqrzd <4950876+xqrzd@users.noreply.github.com>
2020-05-16 00:16:51 -0400

    Improve type validation for RowResult

--- src/Knet.Kudu.Client/Util/KuduEncoder.cs
+++ src/Knet.Kudu.Client/Util/KuduEncoder.cs
@@ -204,11 +204,11 @@ namespace Knet.Kudu.Client.Util
             };
         }
 
-        public static bool DecodeBool(ReadOnlySpan<byte> source) =>
-            source[0] > 0;
+        public static bool DecodeBool(ReadOnlySpan<byte> source) => source[0] > 0;
 
-        public static sbyte DecodeInt8(ReadOnlySpan<byte> source) =>
-            (sbyte)source[0];
+        public static sbyte DecodeInt8(ReadOnlySpan<byte> source) => (sbyte)source[0];
+
+        public static byte DecodeUInt8(ReadOnlySpan<byte> source) => source[0];
 
         public static short DecodeInt16(ReadOnlySpan<byte> source) =>
             BinaryPrimitives.ReadInt16LittleEndian(source);
@@ -263,12 +263,9 @@ namespace Knet.Kudu.Client.Util
                     long longVal = DecodeInt64(source);
                     return DecimalUtil.DecodeDecimal64(longVal, scale);
 
-                case KuduType.Decimal128:
+                default:
                     KuduInt128 int128Val = DecodeInt128(source);
                     return DecimalUtil.DecodeDecimal128(int128Val, scale);
-
-                default:
-                    throw new Exception($"Unsupported data type: {kuduType}.");
             }
         }
 
