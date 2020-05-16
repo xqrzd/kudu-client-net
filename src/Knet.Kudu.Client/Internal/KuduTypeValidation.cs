1f2f08817f3adc89cb6c685c94ca1cb8e621bdcc
xqrzd <4950876+xqrzd@users.noreply.github.com>
2020-05-16 00:24:37 -0400

    oops

---
+++ src/Knet.Kudu.Client/Internal/KuduTypeValidation.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Knet.Kudu.Client.Internal
+{
+    public static class KuduTypeValidation
+    {
+        public static bool IsOfType(this KuduType type, KuduTypeFlags types)
+        {
+            uint typeFlag = (uint)1 << (int)type;
+            return (typeFlag & (uint)types) != 0;
+        }
+
+        public static void ValidateColumnType(
+            this ColumnSchema column, KuduTypeFlags types)
+        {
+            if (!column.Type.IsOfType(types))
+                ThrowException(column, types);
+        }
+
+        public static void ValidateColumnType(
+            this ColumnSchema column, KuduType type)
+        {
+            if (column.Type != type)
+                ThrowException(column, type);
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void ThrowException(ColumnSchema column, KuduTypeFlags types)
+        {
+            throw new ArgumentException(
+                $"Expected column {column} to be one of ({types})");
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static T ThrowException<T>(ColumnSchema column, KuduTypeFlags types)
+        {
+            throw new ArgumentException(
+                $"Expected column {column} to be one of ({types})");
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void ThrowException(ColumnSchema column, KuduType type)
+        {
+            throw new ArgumentException(
+                $"Expected column {column} to be of {type}");
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void ThrowNullException(ColumnSchema column)
+        {
+            throw new Exception($"Column {column} is null");
+        }
+    }
+}
