0b43c54bf1eb9767fd9a3f1dfd4fc5dd0b0aac3a
xqrzd <4950876+xqrzd@users.noreply.github.com>
2020-05-18 20:17:19 -0400

    Simplify SystemClock

--- src/Knet.Kudu.Client/Internal/SystemClock.cs
+++ src/Knet.Kudu.Client/Internal/SystemClock.cs
@@ -3,17 +3,14 @@ using System.Diagnostics;
 
 namespace Knet.Kudu.Client.Internal
 {
+    public class SystemClock : ISystemClock
+    {
 #if NETCOREAPP3_0
-    public class SystemClock : ISystemClock
-    {
         public long CurrentMilliseconds => Environment.TickCount64;
-    }
 #else
-    public class SystemClock : ISystemClock
-    {
         private readonly Stopwatch _stopwatch = Stopwatch.StartNew();
 
         public long CurrentMilliseconds => _stopwatch.ElapsedMilliseconds;
-    }
 #endif
+    }
 }
