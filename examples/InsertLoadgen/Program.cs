dbb15c00f5c8297dcf5e88c09579240d13c61286
xqrzd <4950876+xqrzd@users.noreply.github.com>
2020-06-01 20:43:35 -0400

    Add VS Code support (#100)
    Add VS Code support [part 1]

---
+++ examples/InsertLoadgen/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Knet.Kudu.Client;
+using Microsoft.Extensions.Logging;
+
+namespace InsertLoadgen
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var loggerFactory = LoggerFactory.Create(builder =>
+            {
+                builder
+                    .SetMinimumLevel(LogLevel.Trace)
+                    .AddConsole();
+            });
+
+            await using var client = KuduClient.NewBuilder("localhost:7051")
+                .SetLoggerFactory(loggerFactory)
+                .Build();
+        }
+    }
+}
