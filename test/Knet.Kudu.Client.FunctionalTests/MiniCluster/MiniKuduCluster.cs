2d3185dfc6383b0133917351fdec5f4743a27c41
xqrzd <4950876+xqrzd@users.noreply.github.com>
2020-06-03 23:53:57 -0400

    Remove debug log

--- test/Knet.Kudu.Client.FunctionalTests/MiniCluster/MiniKuduCluster.cs
+++ test/Knet.Kudu.Client.FunctionalTests/MiniCluster/MiniKuduCluster.cs
@@ -69,7 +69,6 @@ namespace Knet.Kudu.Client.FunctionalTests.MiniCluster
 
             foreach (var env in kuduExe.EnvironmentVariables)
             {
-                Console.WriteLine($"Key: {env.Key}, Value: {env.Value}");
                 startInfo.EnvironmentVariables.Add(env.Key, env.Value);
             }
 
