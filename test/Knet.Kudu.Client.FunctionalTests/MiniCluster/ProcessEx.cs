ae7f69e21742474baaec8cf1d9d5529303c2b46d
xqrzd <4950876+xqrzd@users.noreply.github.com>
2021-07-01 20:42:00 -0400

    Simplify ProcessEx

--- test/Knet.Kudu.Client.FunctionalTests/MiniCluster/ProcessEx.cs
+++ test/Knet.Kudu.Client.FunctionalTests/MiniCluster/ProcessEx.cs
@@ -1,33 +1,24 @@
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Knet.Kudu.Client.FunctionalTests.MiniCluster
 {
     /// <summary>
     /// From CliWrap
-    /// https://github.com/Tyrrrz/CliWrap/blob/master/CliWrap/Internal/ProcessEx.cs
+    /// https://github.com/Tyrrrz/CliWrap/blob/master/CliWrap/Utils/ProcessEx.cs
     /// </summary>
     public class ProcessEx : IDisposable
     {
         private readonly Process _nativeProcess;
-        private readonly TaskCompletionSource<object> _exitTcs = new TaskCompletionSource<object>(
-            TaskCreationOptions.RunContinuationsAsynchronously);
-
-        public int Id { get; private set; }
 
         public Stream StdIn { get; private set; }
 
         public Stream StdOut { get; private set; }
 
         public Stream StdErr { get; private set; }
-
-        public int ExitCode { get; private set; }
-
-        public DateTimeOffset StartTime { get; private set; }
-
-        public DateTimeOffset ExitTime { get; private set; }
 
         public ProcessEx(ProcessStartInfo startInfo)
         {
@@ -36,50 +27,20 @@ namespace Knet.Kudu.Client.FunctionalTests.MiniCluster
 
         public void Start()
         {
-            _nativeProcess.EnableRaisingEvents = true;
-            _nativeProcess.Exited += (sender, args) =>
-            {
-                ExitTime = DateTimeOffset.Now;
-                ExitCode = _nativeProcess.ExitCode;
-                _exitTcs.TrySetResult(null);
-            };
-
             _nativeProcess.Start();
-            StartTime = DateTimeOffset.Now;
 
-            Id = _nativeProcess.Id;
             StdIn = _nativeProcess.StandardInput.BaseStream;
             StdOut = _nativeProcess.StandardOutput.BaseStream;
             StdErr = _nativeProcess.StandardError.BaseStream;
         }
 
-        public bool TryKill()
+        public Task WaitForExitAsync(CancellationToken cancellationToken = default)
         {
-            try
-            {
-                _nativeProcess.EnableRaisingEvents = false;
-                _nativeProcess.Kill(true);
-
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
-            finally
-            {
-                _exitTcs.TrySetCanceled();
-            }
+            return _nativeProcess.WaitForExitAsync(cancellationToken);
         }
-
-        public Task WaitUntilExitAsync() => _exitTcs.Task;
 
         public void Dispose()
         {
-            // Kill the process if it's still alive by this point.
-            if (!_nativeProcess.HasExited)
-                TryKill();
-
             _nativeProcess.Dispose();
         }
     }
