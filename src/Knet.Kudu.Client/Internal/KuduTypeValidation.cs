65072152506a5891ba1c7cfe0e794c2d12ea7754
xqrzd <4950876+xqrzd@users.noreply.github.com>
2021-08-29 23:36:28 -0400

    Update some exceptions

--- src/Knet.Kudu.Client/Internal/KuduTypeValidation.cs
+++ src/Knet.Kudu.Client/Internal/KuduTypeValidation.cs
@@ -5,12 +5,14 @@ namespace Knet.Kudu.Client.Internal
 {
     public static class KuduTypeValidation
     {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static bool IsOfType(this KuduType type, KuduTypeFlags types)
         {
-            uint typeFlag = (uint)1 << (int)type;
-            return (typeFlag & (uint)types) != 0;
+            int typeFlag = 1 << (int)type;
+            return (typeFlag & (int)types) != 0;
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void ValidateColumnType(
             this ColumnSchema column, KuduTypeFlags types)
         {
@@ -18,6 +20,7 @@ namespace Knet.Kudu.Client.Internal
                 ThrowException(column, types);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void ValidateColumnType(
             this ColumnSchema column, KuduType type)
         {
@@ -25,46 +28,42 @@ namespace Knet.Kudu.Client.Internal
                 ThrowException(column, type);
         }
 
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void ValidateColumnIsFixedLengthType(this ColumnSchema column)
         {
             if (!column.IsFixedSize)
                 ThrowNotFixedLengthException(column);
         }
 
-        [MethodImpl(MethodImplOptions.NoInlining)]
         public static void ThrowException(ColumnSchema column, KuduTypeFlags types)
         {
             throw new ArgumentException(
                 $"Expected column {column} to be one of ({types})");
         }
 
-        [MethodImpl(MethodImplOptions.NoInlining)]
+        // TODO: Remove this method
         public static T ThrowException<T>(ColumnSchema column, KuduTypeFlags types)
         {
             throw new ArgumentException(
                 $"Expected column {column} to be one of ({types})");
         }
 
-        [MethodImpl(MethodImplOptions.NoInlining)]
         public static void ThrowException(ColumnSchema column, KuduType type)
         {
             throw new ArgumentException(
                 $"Expected column {column} to be of {type}");
         }
 
-        [MethodImpl(MethodImplOptions.NoInlining)]
         public static void ThrowNullException(ColumnSchema column)
         {
             throw new Exception($"Column {column} is null");
         }
 
-        [MethodImpl(MethodImplOptions.NoInlining)]
         public static void ThrowNotNullableException(ColumnSchema column)
         {
             throw new Exception($"Column {column} is not nullable");
         }
 
-        [MethodImpl(MethodImplOptions.NoInlining)]
         public static void ThrowNotFixedLengthException(ColumnSchema column)
         {
             throw new Exception($"Column {column} is not a fixed length type");
