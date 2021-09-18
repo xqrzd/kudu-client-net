b8afa6242ddb39af591c4571d383c8f14d477004
xqrzd <4950876+xqrzd@users.noreply.github.com>
2021-09-17 20:51:28 -0400

    Use Random.Shared on .NET 6

--- src/Knet.Kudu.Client/Internal/ThreadSafeRandom.cs
+++ src/Knet.Kudu.Client/Internal/ThreadSafeRandom.cs
@@ -5,11 +5,15 @@ namespace Knet.Kudu.Client.Internal
 {
     internal static class ThreadSafeRandom
     {
+#if NET6_0_OR_GREATER
+        public static Random Instance => Random.Shared;
+#else
         private static int _seed = Environment.TickCount;
 
         private static readonly ThreadLocal<Random> _random =
             new(() => new Random(Interlocked.Increment(ref _seed)));
 
         public static Random Instance => _random.Value;
+#endif
     }
 }
