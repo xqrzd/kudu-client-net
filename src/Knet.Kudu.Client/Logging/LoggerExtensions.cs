3545a97f6ac210b48398fbbae3f50e4254201615
xqrzd <4950876+xqrzd@users.noreply.github.com>
2021-11-18 00:25:06 -0500

    Use file-scoped namespaces in LoggerExtensions

--- src/Knet.Kudu.Client/Logging/LoggerExtensions.cs
+++ src/Knet.Kudu.Client/Logging/LoggerExtensions.cs
@@ -3,115 +3,111 @@ using System.Net;
 using Knet.Kudu.Client.Connection;
 using Microsoft.Extensions.Logging;
 
-// TODO: Use file-scoped namespaces on .NET 6 RC2
-#pragma warning disable IDE0161 // Convert to file-scoped namespace
-namespace Knet.Kudu.Client.Logging
-#pragma warning restore IDE0161 // Convert to file-scoped namespace
+namespace Knet.Kudu.Client.Logging;
+
+internal static partial class LoggerExtensions
 {
-    internal static partial class LoggerExtensions
-    {
-        [LoggerMessage(
-            EventId = 1,
-            Level = LogLevel.Debug,
-            EventName = "ConnectedToServer",
-            Message = "Connected to {HostPort}, Ip: {Ip}, Encryption: {Encryption}, " +
-                      "Authentication: {Authentication}")]
-        public static partial void ConnectedToServer(
-            this ILogger logger,
-            HostAndPort HostPort,
-            IPAddress Ip,
-            string Encryption,
-            string Authentication,
-            string? TlsCipher,
-            string? ServicePrincipalName,
-            bool IsLocal);
+    [LoggerMessage(
+        EventId = 1,
+        Level = LogLevel.Debug,
+        EventName = "ConnectedToServer",
+        Message = "Connected to {HostPort}, Ip: {Ip}, Encryption: {Encryption}, " +
+                  "Authentication: {Authentication}")]
+    public static partial void ConnectedToServer(
+        this ILogger logger,
+        HostAndPort HostPort,
+        IPAddress Ip,
+        string Encryption,
+        string Authentication,
+        string? TlsCipher,
+        string? ServicePrincipalName,
+        bool IsLocal);
 
-        [LoggerMessage(
-            EventId = 2,
-            Level = LogLevel.Warning,
-            EventName = "MisconfiguredMasterAddresses",
-            Message = "Client configured with {NumClientMasters} master(s) but cluster " +
-                      "indicates it expects {NumClusterMasters} master(s)")]
-        public static partial void MisconfiguredMasterAddresses(
-            this ILogger logger,
-            int NumClientMasters,
-            int NumClusterMasters,
-            string ClientMasters,
-            string ClusterMasters);
+    [LoggerMessage(
+        EventId = 2,
+        Level = LogLevel.Warning,
+        EventName = "MisconfiguredMasterAddresses",
+        Message = "Client configured with {NumClientMasters} master(s) but cluster " +
+                  "indicates it expects {NumClusterMasters} master(s)")]
+    public static partial void MisconfiguredMasterAddresses(
+        this ILogger logger,
+        int NumClientMasters,
+        int NumClusterMasters,
+        string ClientMasters,
+        string ClusterMasters);
 
-        [LoggerMessage(
-            EventId = 3,
-            Level = LogLevel.Warning,
-            EventName = "ConnectionDisconnected",
-            Message = "Connection to {Server} closed ungracefully")]
-        public static partial void ConnectionDisconnected(
-            this ILogger logger,
-            Exception exception,
-            string Server);
+    [LoggerMessage(
+        EventId = 3,
+        Level = LogLevel.Warning,
+        EventName = "ConnectionDisconnected",
+        Message = "Connection to {Server} closed ungracefully")]
+    public static partial void ConnectionDisconnected(
+        this ILogger logger,
+        Exception exception,
+        string Server);
 
-        [LoggerMessage(
-            EventId = 4,
-            Level = LogLevel.Warning,
-            EventName = "UnableToConnectToServer",
-            Message = "Unable to connect to {HostPort}, Ip: {Ip}, UUID: {Uuid}")]
-        public static partial void UnableToConnectToServer(
-            this ILogger logger,
-            Exception exception,
-            HostAndPort HostPort,
-            IPAddress Ip,
-            string Uuid);
+    [LoggerMessage(
+        EventId = 4,
+        Level = LogLevel.Warning,
+        EventName = "UnableToConnectToServer",
+        Message = "Unable to connect to {HostPort}, Ip: {Ip}, UUID: {Uuid}")]
+    public static partial void UnableToConnectToServer(
+        this ILogger logger,
+        Exception exception,
+        HostAndPort HostPort,
+        IPAddress Ip,
+        string Uuid);
 
-        [LoggerMessage(
-            EventId = 5,
-            Level = LogLevel.Error,
-            EventName = "ExceptionFlushingSessionData",
-            Message = "Session failed to flush {NumRows} rows")]
-        public static partial void ExceptionFlushingSessionData(
-            this ILogger logger,
-            Exception exception,
-            int NumRows,
-            long TxnId);
+    [LoggerMessage(
+        EventId = 5,
+        Level = LogLevel.Error,
+        EventName = "ExceptionFlushingSessionData",
+        Message = "Session failed to flush {NumRows} rows")]
+    public static partial void ExceptionFlushingSessionData(
+        this ILogger logger,
+        Exception exception,
+        int NumRows,
+        long TxnId);
 
-        [LoggerMessage(
-            EventId = 6,
-            Level = LogLevel.Warning,
-            EventName = "RecoverableRpcException",
-            Message = "RPC attempt {Attempt} failed, will retry")]
-        public static partial void RecoverableRpcException(
-            this ILogger logger,
-            Exception exception,
-            int Attempt);
+    [LoggerMessage(
+        EventId = 6,
+        Level = LogLevel.Warning,
+        EventName = "RecoverableRpcException",
+        Message = "RPC attempt {Attempt} failed, will retry")]
+    public static partial void RecoverableRpcException(
+        this ILogger logger,
+        Exception exception,
+        int Attempt);
 
-        [LoggerMessage(
-            EventId = 7,
-            Level = LogLevel.Warning,
-            EventName = "ScannerExpired",
-            Message = "Scanner {ScannerId} for {Table} expired, creating a new one")]
-        public static partial void ScannerExpired(
-            this ILogger logger,
-            string? ScannerId,
-            string Table,
-            string? TabletId);
+    [LoggerMessage(
+        EventId = 7,
+        Level = LogLevel.Warning,
+        EventName = "ScannerExpired",
+        Message = "Scanner {ScannerId} for {Table} expired, creating a new one")]
+    public static partial void ScannerExpired(
+        this ILogger logger,
+        string? ScannerId,
+        string Table,
+        string? TabletId);
 
-        [LoggerMessage(
-            EventId = 8,
-            Level = LogLevel.Warning,
-            EventName = "ReceivedUnknownRpc",
-            Message = "Received unknown RPC call ID {CallId} from {Server}")]
-        public static partial void ReceivedUnknownRpc(
-            this ILogger logger,
-            int CallId,
-            string Server);
+    [LoggerMessage(
+        EventId = 8,
+        Level = LogLevel.Warning,
+        EventName = "ReceivedUnknownRpc",
+        Message = "Received unknown RPC call ID {CallId} from {Server}")]
+    public static partial void ReceivedUnknownRpc(
+        this ILogger logger,
+        int CallId,
+        string Server);
 
-        [LoggerMessage(
-            EventId = 9,
-            Level = LogLevel.Information,
-            EventName = "TransactionKeepaliveTerminated",
-            Message = "Transaction keepalive terminated for TxnId {TxnId}")]
-        public static partial void TransactionKeepaliveTerminated(
-            this ILogger logger,
-            Exception exception,
-            long TxnId,
-            TimeSpan KeepaliveInterval);
-    }
+    [LoggerMessage(
+        EventId = 9,
+        Level = LogLevel.Information,
+        EventName = "TransactionKeepaliveTerminated",
+        Message = "Transaction keepalive terminated for TxnId {TxnId}")]
+    public static partial void TransactionKeepaliveTerminated(
+        this ILogger logger,
+        Exception exception,
+        long TxnId,
+        TimeSpan KeepaliveInterval);
 }
