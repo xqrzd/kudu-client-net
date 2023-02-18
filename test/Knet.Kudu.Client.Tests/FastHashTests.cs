e3df34396a9b7a56778041bd4907b190b1b86610
xqrzd <4950876+xqrzd@users.noreply.github.com>
2023-02-18 15:16:47 -0500

    Special case empty strings in FastHash

--- test/Knet.Kudu.Client.Tests/FastHashTests.cs
+++ test/Knet.Kudu.Client.Tests/FastHashTests.cs
@@ -7,9 +7,11 @@ namespace Knet.Kudu.Client.Tests;
 public class FastHashTests
 {
     [Theory]
-    [InlineData("ab", 0, 17293172613997361769UL)]
-    [InlineData("abcdefg", 0, 10206404559164245992UL)]
-    [InlineData("quick brown fox", 42, 3757424404558187042UL)]
+    [InlineData("ab", 0, 17293172613997361769)]
+    [InlineData("abcdefg", 0, 10206404559164245992)]
+    [InlineData("quick brown fox", 42, 3757424404558187042)]
+    [InlineData("", 0, 4144680785095980158)]
+    [InlineData("", 1234, 3296774803014270295)]
     public void TestFastHash64(string data, ulong seed, ulong expectedHash)
     {
         ulong hash = FastHash.Hash64(data.ToUtf8ByteArray(), seed);
@@ -17,9 +19,11 @@ public class FastHashTests
     }
 
     [Theory]
-    [InlineData("ab", 0, 2564147595U)]
-    [InlineData("abcdefg", 0, 1497700618U)]
-    [InlineData("quick brown fox", 42, 1676541068U)]
+    [InlineData("ab", 0, 2564147595)]
+    [InlineData("abcdefg", 0, 1497700618)]
+    [InlineData("quick brown fox", 42, 1676541068)]
+    [InlineData("", 0, 3045300040)]
+    [InlineData("", 1234, 811548192)]
     public void TestFastHash32(string data, uint seed, uint expectedHash)
     {
         uint hash = FastHash.Hash32(data.ToUtf8ByteArray(), seed);
