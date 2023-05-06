779d602b435e4d47d98d231945b7bb115adf89ab
xqrzd <4950876+xqrzd@users.noreply.github.com>
2023-05-06 18:50:03 -0400

    Add Find methods

--- src/Knet.Kudu.Client/KuduBloomFilter.cs
+++ src/Knet.Kudu.Client/KuduBloomFilter.cs
@@ -39,90 +39,222 @@ public class KuduBloomFilter
 
     public void AddBool(bool value)
     {
-        CheckType(KuduType.Bool);
-        Span<byte> span = stackalloc byte[1];
-        KuduEncoder.EncodeBool(span, value);
-        AddSpan(span);
+        uint hash = GetBoolHash(value);
+        BloomFilter.Insert(hash);
     }
 
     public void AddSByte(sbyte value)
     {
-        CheckType(KuduType.Int8);
-        Span<byte> span = stackalloc byte[1];
-        KuduEncoder.EncodeInteger(span, value);
-        AddSpan(span);
+        uint hash = GetSByteHash(value);
+        BloomFilter.Insert(hash);
     }
 
     public void AddInt16(short value)
     {
-        CheckType(KuduType.Int16);
-        Span<byte> span = stackalloc byte[2];
-        KuduEncoder.EncodeInteger(span, value);
-        AddSpan(span);
+        uint hash = GetInt16Hash(value);
+        BloomFilter.Insert(hash);
     }
 
     public void AddInt32(int value)
     {
-        CheckType(KuduTypeFlags.Int32 | KuduTypeFlags.Date);
-        Span<byte> span = stackalloc byte[4];
-        KuduEncoder.EncodeInteger(span, value);
-        AddSpan(span);
+        uint hash = GetInt32Hash(value);
+        BloomFilter.Insert(hash);
     }
 
     public void AddInt64(long value)
     {
-        CheckType(KuduTypeFlags.Int64 | KuduTypeFlags.UnixtimeMicros);
-        Span<byte> span = stackalloc byte[8];
-        KuduEncoder.EncodeInteger(span, value);
-        AddSpan(span);
+        uint hash = GetInt64Hash(value);
+        BloomFilter.Insert(hash);
     }
 
     public void AddDateTime(DateTime value)
     {
-        var type = Column.Type;
-
-        if (type == KuduType.UnixtimeMicros)
-        {
-            Span<byte> span = stackalloc byte[8];
-            KuduEncoder.EncodeDateTime(span, value);
-            AddSpan(span);
-        }
-        else if (type == KuduType.Date)
-        {
-            Span<byte> span = stackalloc byte[4];
-            KuduEncoder.EncodeDate(span, value);
-            AddSpan(span);
-        }
-        else
-        {
-            KuduTypeValidation.ThrowException(Column,
-                KuduTypeFlags.UnixtimeMicros |
-                KuduTypeFlags.Date);
-        }
+        uint hash = GetDateTimeHash(value);
+        BloomFilter.Insert(hash);
     }
 
     // TODO: DateOnly
 
     public void AddFloat(float value)
     {
-        CheckType(KuduType.Float);
-        Span<byte> span = stackalloc byte[4];
-        KuduEncoder.EncodeFloat(span, value);
-        AddSpan(span);
+        uint hash = GetFloatHash(value);
+        BloomFilter.Insert(hash);
     }
 
     public void AddDouble(double value)
     {
-        CheckType(KuduType.Double);
-        Span<byte> span = stackalloc byte[8];
-        KuduEncoder.EncodeDouble(span, value);
-        AddSpan(span);
+        uint hash = GetDoubleHash(value);
+        BloomFilter.Insert(hash);
     }
 
     public void AddDecimal(decimal value)
     {
-        CheckType(KuduTypeFlags.Decimal32 | KuduTypeFlags.Decimal64 | KuduTypeFlags.Decimal128);
+        uint hash = GetDecimalHash(value);
+        BloomFilter.Insert(hash);
+    }
 
+    public void AddString(string value)
+    {
+        uint hash = GetStringHash(value);
+        BloomFilter.Insert(hash);
+    }
+
+    public void AddBinary(ReadOnlySpan<byte> value)
+    {
+        uint hash = GetBinaryHash(value);
+        BloomFilter.Insert(hash);
+    }
+
+    public bool FindBool(bool value)
+    {
+        uint hash = GetBoolHash(value);
+        return BloomFilter.Find(hash);
+    }
+
+    public bool FindSByte(sbyte value)
+    {
+        uint hash = GetSByteHash(value);
+        return BloomFilter.Find(hash);
+    }
+
+    public bool FindInt16(short value)
+    {
+        uint hash = GetInt16Hash(value);
+        return BloomFilter.Find(hash);
+    }
+
+    public bool FindInt32(int value)
+    {
+        uint hash = GetInt32Hash(value);
+        return BloomFilter.Find(hash);
+    }
+
+    public bool FindInt64(long value)
+    {
+        uint hash = GetInt64Hash(value);
+        return BloomFilter.Find(hash);
+    }
+
+    public bool FindDateTime(DateTime value)
+    {
+        uint hash = GetDateTimeHash(value);
+        return BloomFilter.Find(hash);
+    }
+
+    public bool FindFloat(float value)
+    {
+        uint hash = GetFloatHash(value);
+        return BloomFilter.Find(hash);
+    }
+
+    public bool FindDouble(double value)
+    {
+        uint hash = GetDoubleHash(value);
+        return BloomFilter.Find(hash);
+    }
+
+    public bool FindDecimal(decimal value)
+    {
+        uint hash = GetDecimalHash(value);
+        return BloomFilter.Find(hash);
+    }
+
+    public bool FindString(string value)
+    {
+        uint hash = GetStringHash(value);
+        return BloomFilter.Find(hash);
+    }
+
+    public bool FindBinary(ReadOnlySpan<byte> value)
+    {
+        uint hash = GetBinaryHash(value);
+        return BloomFilter.Find(hash);
+    }
+
+    private uint GetBoolHash(bool value)
+    {
+        CheckType(KuduType.Bool);
+        Span<byte> span = stackalloc byte[1];
+        KuduEncoder.EncodeBool(span, value);
+        return FastHash.Hash32(span, _hashSeed);
+    }
+
+    private uint GetSByteHash(sbyte value)
+    {
+        CheckType(KuduType.Int8);
+        Span<byte> span = stackalloc byte[1];
+        KuduEncoder.EncodeInteger(span, value);
+        return FastHash.Hash32(span, _hashSeed);
+    }
+
+    private uint GetInt16Hash(short value)
+    {
+        CheckType(KuduType.Int16);
+        Span<byte> span = stackalloc byte[2];
+        KuduEncoder.EncodeInteger(span, value);
+        return FastHash.Hash32(span, _hashSeed);
+    }
+
+    private uint GetInt32Hash(int value)
+    {
+        CheckType(KuduTypeFlags.Int32 | KuduTypeFlags.Date);
+        Span<byte> span = stackalloc byte[4];
+        KuduEncoder.EncodeInteger(span, value);
+        return FastHash.Hash32(span, _hashSeed);
+    }
+
+    private uint GetInt64Hash(long value)
+    {
+        CheckType(KuduTypeFlags.Int64 | KuduTypeFlags.UnixtimeMicros);
+        Span<byte> span = stackalloc byte[8];
+        KuduEncoder.EncodeInteger(span, value);
+        return FastHash.Hash32(span, _hashSeed);
+    }
+
+    private uint GetDateTimeHash(DateTime value)
+    {
+        var column = Column;
+
+        switch (column.Type)
+        {
+            case KuduType.UnixtimeMicros:
+                {
+                    Span<byte> span = stackalloc byte[8];
+                    KuduEncoder.EncodeDateTime(span, value);
+                    return FastHash.Hash32(span, _hashSeed);
+                }
+            case KuduType.Date:
+                {
+                    Span<byte> span = stackalloc byte[4];
+                    KuduEncoder.EncodeDate(span, value);
+                    return FastHash.Hash32(span, _hashSeed);
+                }
+            default:
+                {
+                    return KuduTypeValidation.ThrowException<uint>(column,
+                        KuduTypeFlags.UnixtimeMicros | KuduTypeFlags.Date);
+                }
+        }
+    }
+
+    private uint GetFloatHash(float value)
+    {
+        CheckType(KuduType.Float);
+        Span<byte> span = stackalloc byte[4];
+        KuduEncoder.EncodeFloat(span, value);
+        return FastHash.Hash32(span, _hashSeed);
+    }
+
+    private uint GetDoubleHash(double value)
+    {
+        CheckType(KuduType.Double);
+        Span<byte> span = stackalloc byte[8];
+        KuduEncoder.EncodeDouble(span, value);
+        return FastHash.Hash32(span, _hashSeed);
+    }
+
+    private uint GetDecimalHash(decimal value)
+    {
         var column = Column;
         var attributes = column.TypeAttributes!;
         var precision = attributes.Precision.GetValueOrDefault();
@@ -134,27 +266,30 @@ public class KuduBloomFilter
                 {
                     Span<byte> span = stackalloc byte[4];
                     KuduEncoder.EncodeDecimal32(span, value, precision, scale);
-                    AddSpan(span);
-                    break;
+                    return FastHash.Hash32(span, _hashSeed);
                 }
             case KuduType.Decimal64:
                 {
                     Span<byte> span = stackalloc byte[8];
                     KuduEncoder.EncodeDecimal64(span, value, precision, scale);
-                    AddSpan(span);
-                    break;
+                    return FastHash.Hash32(span, _hashSeed);
                 }
             case KuduType.Decimal128:
                 {
                     Span<byte> span = stackalloc byte[16];
                     KuduEncoder.EncodeDecimal128(span, value, precision, scale);
-                    AddSpan(span);
-                    break;
+                    return FastHash.Hash32(span, _hashSeed);
                 }
+            default:
+                {
+                    return KuduTypeValidation.ThrowException<uint>(column,
+                        KuduTypeFlags.Decimal32 | KuduTypeFlags.Decimal64 | KuduTypeFlags.Decimal128);
+                }
+
         }
     }
 
-    public void AddString(string value)
+    private uint GetStringHash(string value)
     {
         // TODO: Substring varchar?
         CheckType(KuduTypeFlags.String | KuduTypeFlags.Varchar);
@@ -168,40 +303,21 @@ public class KuduBloomFilter
             : (unescapedArray = ArrayPool<byte>.Shared.Rent(length));
 
         var written = Encoding.UTF8.GetBytes(value, span);
-        AddSpan(span.Slice(0, written));
+        var slice = span.Slice(0, written);
+        uint hash = FastHash.Hash32(slice, _hashSeed);
 
         if (unescapedArray is not null)
         {
             ArrayPool<byte>.Shared.Return(unescapedArray);
         }
+
+        return hash;
     }
 
-    public void AddBinary(ReadOnlySpan<byte> value)
+    private uint GetBinaryHash(ReadOnlySpan<byte> value)
     {
         CheckType(KuduType.Binary);
-        AddSpan(value);
-    }
-
-    // TODO: Add remaining find methods
-
-    public bool FindInt64(long value)
-    {
-        Span<byte> span = stackalloc byte[8];
-        KuduEncoder.EncodeInteger(span, value);
-        uint hash = FastHash.Hash32(span, _hashSeed);
-        return BloomFilter.Find(hash);
-    }
-
-    public bool FindBinary(ReadOnlySpan<byte> value)
-    {
-        uint hash = FastHash.Hash32(value, _hashSeed);
-        return BloomFilter.Find(hash);
-    }
-
-    private void AddSpan(ReadOnlySpan<byte> value)
-    {
-        uint hash = FastHash.Hash32(value, _hashSeed);
-        BloomFilter.Insert(hash);
+        return FastHash.Hash32(value, _hashSeed);
     }
 
     private void CheckType(KuduType type)
